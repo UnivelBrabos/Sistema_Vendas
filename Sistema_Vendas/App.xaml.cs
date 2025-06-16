fb490f9d7c10726e86ca510a69f0066d3b9ceba4
Carlos Eduardo Menegassi <161506037+CarlosEM007@users.noreply.github.com>
2025-06-16 19:47:58 -0300

    Criando PullRequest.

--- Sistema_Vendas/App.xaml.cs
+++ Sistema_Vendas/App.xaml.cs
@@ -6,7 +6,7 @@ using Sistema_Vendas.Model.FilteredModel;
 namespace Sistema_Vendas
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
